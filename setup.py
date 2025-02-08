970fe96f962762fa1b8759342f2f84223739c449
RIK <info@trify.sk>
2025-02-08 12:43:25 +0100

    Fixed cx_Freeze setup configuration_v1-12

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import sys
 from cx_Freeze import setup, Executable
+import os
+
+# Získanie absolútnej cesty k súborom
+manifest_file = os.path.abspath("app.manifest")
+version_file = os.path.abspath("version_info.txt")
 
 build_exe_options = {
     "packages": ["os", "requests", "PyQt5", "json", "datetime", "platform"],
     "excludes": [],
-    "include_files": ["version_info.txt", "app.manifest"]
+    "include_files": [],  # Odstránime include_files, ktoré spôsobujú problém
+    "include_msvcr": True
 }
+
+base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
     name = "VideoScheduler",
@@ -14,7 +22,7 @@ setup(
     options = {"build_exe": build_exe_options},
     executables = [Executable(
         "studio-video_scheduler.py",
-        base="Win32GUI",
+        base=base,
         target_name="VideoScheduler.exe",
         copyright="Copyright (c) 2024 TRIFY s.r.o.",
         trademarks="TRIFY s.r.o."
